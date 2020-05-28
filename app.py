aaedfe643b17abc4ec12615b9822220ae1a368e0
Matheus-FC <matheus.cinachi@gmail.com>
2020-05-28 12:26:57 -0300

    Add CORS

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask_restful import Api
 from database.db import initialize_db
 from resources.routes import initialize_routes
+from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
 app.config['MONGODB_SETTINGS'] = {
     'db': 'Cluster0',
     'host': 'mongodb+srv://piAdmin:pi1234@cluster0-vpcqm.gcp.mongodb.net/test?retryWrites=true&w=majority',
