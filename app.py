6472bb804609ec51285660a905eb9a8c41cef873
Matheus-FC <matheus.cinachi@gmail.com>
2020-06-13 20:52:01 -0300

    Changed host port

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app)
-CORS(app)
+CORS(api)
 app.config['MONGODB_SETTINGS'] = {
     'db': 'Cluster0',
     'host': 'mongodb+srv://piAdmin:pi1234@cluster0-vpcqm.gcp.mongodb.net/test?retryWrites=true&w=majority',
@@ -16,4 +16,4 @@ initialize_db(app)
 initialize_routes(api)
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=4002, debug=True)
