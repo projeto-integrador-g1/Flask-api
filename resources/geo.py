9d0a5805ffb59cd7965f28aa1afa7f18e7df1ed2
Matheus Froes Cinachi <matheus.cinachi@gmail.com>
2020-07-08 19:49:31 -0300

    Feature Extract Zip Files

--- resources/geo.py
+++ resources/geo.py
@@ -7,6 +7,10 @@ import shapefile as shp
 from osgeo import ogr
 import os
 import json
+import fiona
+import rasterio
+import rasterio.mask
+from zipfile import ZipFile
 import requests
 from pymongo import MongoClient
 client = MongoClient('mongodb+srv://piAdmin:pi1234@cluster0-vpcqm.gcp.mongodb.net/test?retryWrites=true&w=majority', 27017)
@@ -39,7 +43,7 @@ class GeoRequest(Resource):
             x = float(x)
             y = float(y)
             coord.append([y, x])
-        print(coord)
+        #print(coord)
         r = getImages(body["date"], body["satelite"], body["cloudCouverage"], body["geo_coord"])
         return r,200
     
@@ -67,13 +71,13 @@ class AIRequest(Resource):
         req = body['images']
         r = getToAi(req)
         global coord, antcoord
-        antcoord = coord
         print (coord)
         if not coord:
             coord = antcoord
         r.insert(0, coord)
+        antcoord = coord
+        coord = []
         retorno = requests.post('http://127.0.0.1:8922/ia', json=r)
-        del coord[:]
         return Response('', status=200)
     def get(self):
         pload = {'_id':'1234567','Satus':'Done'}
@@ -83,16 +87,14 @@ class AIRequest(Resource):
 class SHPFile(Resource):
     def post(self):
         body = request.values
-        daShapefile = r"C:\Users\mathe\layers\POLYGON.shp"
-        driver = ogr.GetDriverByName('ESRI Shapefile')
-
-        dataSource = driver.Open(daShapefile, 0) # 0 means read-only. 1 means writeable.
-        # Check to see if shapefile is found.
-        if dataSource is None:
-            print ('Could not open %s' % (daShapefile))
-        else:
-            print ('Opened %s' % (daShapefile))
-            layer = dataSource.GetLayer()
-            featureCount = layer.GetFeatureCount()
-            print ("Number of features in %s: %d" % (os.path.basename(daShapefile),featureCount))
-        return Response([], 200)
+        file_name = "file.zip"
+        with ZipFile(file_name, 'r') as zip:
+            zip.printdir()
+            print('aew caralho')
+            zip.extractall()
+            print('foi')
+        with fiona.open("C:/Users/mathe/layers/POLYGON.shp", "r") as shapefile:
+            shapes = [feature["geometry"] for feature in shapefile]
+        vet = shapes[0]['coordinates'][0]
+        print (vet[0])
+        return Response(vet, 200)
